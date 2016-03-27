c191518d62425cca9972b0a6e071cea09ac11e86
Alejandro Huerta <ahuerta0686@gmail.com>
2016-03-27 07:58:23 -0500

    Changes

--- www/js/app.js
+++ www/js/app.js
@@ -20,6 +20,38 @@ angular.module('ItHertz', ['ionic'])
     }
   }
 })
+.factory('VehicleInfo', function () {
+  return {
+    all: function () {
+      var vehicleInfo = window.localStorage['vehicleInfo'];
+      if (vehicleInfo) {
+        return angular.fromJson(vehicleInfo);
+      }
+
+      return null;
+    },
+
+    save: function (vehicleInfo) {
+      window.localStorage['vehicleInfo'] = angular.toJson(vehicleInfo);
+    }
+  }
+})
+.factory('ContactInfo', function () {
+  return {
+    all: function () {
+      var contactInfo = window.localStorage['contactInfo'];
+      if (contactInfo) {
+        return angular.fromJson(contactInfo);
+      }
+
+      return null;
+    },
+
+    save: function (contactInfo) {
+      window.localStorage['contactInfo'] = angular.toJson(contactInfo);
+    }
+  }
+})
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
@@ -43,44 +75,92 @@ angular.module('ItHertz', ['ionic'])
   };
   $scope.acceleration = {};
 
-  // Create our modal
-  $ionicModal.fromTemplateUrl('setup-user.html', function (modal) {
+  // Create our modals
+  $ionicModal.fromTemplateUrl('setup-user-1.html', function (modal) {
     $scope.setupUserModal = modal;
   }, {
     scope: $scope
   });
 
-  $scope.setupUser = function (personalInfo) {
+  $ionicModal.fromTemplateUrl('setup-user-2.html', function (modal) {
+    $scope.setupVehicleModal = modal;
+  }, {
+    scope: $scope
+  });
+
+  $ionicModal.fromTemplateUrl('setup-user-3.html', function (modal) {
+    $scope.setupContactModal = modal;
+  }, {
+    scope: $scope
+  });
+
+  $scope.setupUser = function (personalInfo, skipAll) {
     $scope.setupUserModal.hide();
     PersonalInfo.save(personalInfo);
+
+    if (!skipAll) {
+      $scope.setupVehicleModal.show();
+    }
+  };
+
+  $scope.setupVehicle = function (vehicleInfo, skipAll) {
+    $scope.setupVehicleModal.hide();
+    VehicleInfo.save(vehicleInfo);
+
+    if (!skipAll) {
+      $scope.setupContactModal.show();
+    }
+  };
+
+  $scope.setupContacts = function (contactInfo) {
+    $scope.setupContactModal.hide();
+    ContactInfo.save(contactInfo);
   };
 
   $scope.toggleSidebar = function () {
     $ionicSideMenuDelegate.toggleLeft();
   };
 
-  // Run stuff
-  $timeout(function () {
+  document.addEventListener("deviceready", function () {
     if (!PersonalInfo.all()) {
       $scope.personalInfo = {};
       $scope.setupUserModal.show();
     }
-  });
 
-  document.addEventListener("deviceready", function () {
-    $scope.acceleration.x = "UGH";
-    $scope.acceleration.y = "WHY";
-    
-    navigator.accelerometer.watchAcceleration(
-      function success(result) {
-        $scope.acceleration = result;
-      },
-      function error(error) {
-        $scope.acceleration.x = error;
-      },{
-        frequency: 1000,
-        period: 1000
+    // - NOTIFICATIONS -
+
+    cordova.plugins.notification.local.registerPermission(function (granted) {
+        console.log('Permission has been granted: ' + granted);
+    });
+
+    // - BACKGROUNDING -
+
+    cordova.plugins.backgroundMode.enable();
+
+    // Get informed when the background mode has been activated
+    cordova.plugins.backgroundMode.onactivate = function () {
+        cordova.plugins.notification.local.schedule({
+          id: 1,
+          text: 'Test Message 1'
       });
+    };
+
+    // Get informed when the background mode has been deactivated
+    cordova.plugins.backgroundMode.ondeactivate = function () {
+        clearInterval(timer);
+        // cordova.plugins.notification.badge.clear();
+    };
+
+    // navigator.accelerometer.watchAcceleration(
+    //   function success(result) {
+    //     $scope.acceleration = result;
+    //   },
+    //   function error(error) {
+    //     $scope.acceleration.x = error;
+    //   },{
+    //     frequency: 1000,
+    //     period: 1000
+    //   });
 
     // accelerometer.then(
     //   function () {
@@ -94,6 +174,6 @@ angular.module('ItHertz', ['ionic'])
     //     // $scope.acceleration = angular.copy(result);
     //   });
 
-  }, true);
+  }, false);
   
 });
