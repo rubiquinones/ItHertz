9217b2a567cc732b3f8c1f614641f3135f664ce6
Rubi Quinones <rubi.quinones@hotmail.com>
2016-03-26 19:52:33 -0500

    Add Toggle Button that Runs the Application

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,118 +1,45 @@
-angular.module('todo', ['ionic'])
+angular.module('ItHertz', ['ionic'])
 /**
  * The Projects factory handles saving and loading projects
  * from local storage, and also lets us save and load the
  * last active project index.
  */
-.factory('Projects', function() {
+.factory('PersonalInfo', function () {
   return {
-    all: function() {
-      var projectString = window.localStorage['projects'];
-      if(projectString) {
-        return angular.fromJson(projectString);
+    all: function () {
+      var personalInfo = window.localStorage['personalInfo'];
+      if (personalInfo) {
+        return angular.fromJson(personalInfo);
       }
-      return [];
+
+      return null;
     },
-    save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
-    },
-    newProject: function(projectTitle) {
-      // Add a new project
-      return {
-        title: projectTitle,
-        tasks: []
-      };
-    },
-    getLastActiveIndex: function() {
-      return parseInt(window.localStorage['lastActiveProject']) || 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveProject'] = index;
+
+    save: function (personalInfo) {
+      window.localStorage['personalInfo'] = angular.toJson(personalInfo);
     }
   }
 })
-
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
-
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
-
-
-  // Load or initialize projects
-  $scope.projects = Projects.all();
-
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
-
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
-  };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
+.controller('MainCtrl', function ($scope, $timeout, $ionicModal, PersonalInfo) {
+  // Get status of application
+  $scope.protected = window.localStorage['protected'];
 
   // Create our modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
+  $ionicModal.fromTemplateUrl('setup-user.html', function (modal) {
+    $scope.setupUserModal = modal;
   }, {
     scope: $scope
   });
 
-  $scope.createTask = function(task) {
-    if(!$scope.activeProject || !task) {
-      return;
-    }
-    $scope.activeProject.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
-    task.title = "";
+  $scope.setupUser = function (personalInfo) {
+    $scope.setupUserModal.hide();
+    PersonalInfo.save(personalInfo);
   };
 
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  }
-
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
-        }
-      }
+  $timeout(function () {
+    if (!PersonalInfo.all()) {
+      $scope.personalInfo = {};
+      $scope.setupUserModal.show();
     }
   });
-
 });
