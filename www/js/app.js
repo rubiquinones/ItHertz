4f27ec3e938d52cdf40c61bb69369db807ecd084
Alejandro Huerta <ahuerta0686@gmail.com>
2016-03-27 00:50:05 -0500

    Accelerometer stuff

--- www/js/app.js
+++ www/js/app.js
@@ -23,9 +23,12 @@ angular.module('ItHertz', ['ionic'])
 .controller('MainCtrl', function ($scope, $timeout, $ionicModal, $ionicSideMenuDelegate, PersonalInfo) {
   // Get status of application
   $scope.protected = window.localStorage['protected'];
+
+  // Setup variables
   $scope.app = {
     title: "ItHertz"
   };
+  $scope.acceleration = {};
 
   // Create our modal
   $ionicModal.fromTemplateUrl('setup-user.html', function (modal) {
@@ -40,13 +43,42 @@ angular.module('ItHertz', ['ionic'])
   };
 
   $scope.toggleSidebar = function () {
-    $ionicSideMenuDelegate.toggleLeft();
+    // $ionicSideMenuDelegate.toggleLeft();
   };
 
+  // Run stuff
   $timeout(function () {
     if (!PersonalInfo.all()) {
       $scope.personalInfo = {};
       $scope.setupUserModal.show();
     }
   });
+
+  document.addEventListener("deviceready", function () {
+    $scope.acceleration.x = "UGH";
+    $scope.acceleration.y = "WHY";
+    navigator.accelerometer.watchAcceleration(
+      function success(result) {
+        $scope.acceleration = result;
+      },
+      function error(error) {
+        $scope.acceleration.x = error;
+      },{
+        frequency: 10
+      });
+
+    // accelerometer.then(
+    //   function () {
+    //     $scope.acceleration.y = "idon'tknowhwatimdoing";
+    //   },
+    //   function (error) {
+    //     $scope.acceleration.x = "error";
+    //   },
+    //   function (result) {
+    //     $scope.acceleration.z = "noterror";
+    //     // $scope.acceleration = angular.copy(result);
+    //   });
+
+  }, true);
+  
 });
